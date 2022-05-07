b3164675b95b1f6e1ff756e28ace30085b1d28f8
Afrain Akhter <89746535+afrainakhter@users.noreply.github.com>
2022-05-07 21:52:52 +0600

    voice_part

--- main.py
+++ main.py
@@ -1,40 +1,62 @@
 from cgitb import reset
 from time import ctime
+import time
 import webbrowser
+import playsound
+import os
+import random
+from gtts import gTTS
 import speech_recognition as sr
 
 r = sr.Recognizer()
 def record_audio(ask=False):
     with sr.Microphone()as source:
         if ask:
-            print(ask)
+             robot_speak(ask)
         audio=r.listen(source)
         voice_data=''
         try: 
            voice_data=r.recognize_google(audio)
            
         except sr.UnknownValueError:
-            print("Sorry,I did not get that!")
+            robot_speak("Sorry,I did not get that!")
         except sr.RequestError:
-            print("Sorry,My speech service down")    
+             robot_speak("Sorry,My speech service down")    
         return voice_data
+def robot_speak(audio_string):
+    tts=gTTS(text=audio_string,lang='en')
+    r=random.randint(1,10000000)
+    audio_file='audio-' +str(r)+'.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+
+
+
+
+
+    
 def respond(voice_data):
        if 'what is your name' in voice_data:
-           print('My name is Afrain')
+           robot_speak('My name is Afrain')
        if 'time' in voice_data:
-           print(ctime())
+           robot_speak(ctime())
        if 'search'  in voice_data:
            search=record_audio('What do you   search for?')
            url='https://google.com/search?q=' + search
            webbrowser.get().open(url)
-           print('here is what you search for' +search)
+           robot_speak('here is what you search for' +search)
        if 'find location'  in voice_data:
            location=record_audio('What do you locate for?')
            url='https://google.nl/maps/place/'+location+ '/&amp;'
            webbrowser.get().open(url)
-           print('here is what you locate for' +location)
-         
-    
-print("How can I help You")  
-voice_data=record_audio()
-respond(voice_data)
+           robot_speak('here is what you locate for' +location)
+       if 'exit' in voice_data:
+            exit() 
+time.sleep(1)   
+robot_speak("How can I help You") 
+while 1: 
+   voice_data=record_audio()
+   respond(voice_data)
